af6a7ab6c3ca5bf5c72f97564da2c140a095e3b6
DinhVanNB <phuongvan2430@gmail.com>
2023-01-22 11:23:10 +0700

    add BT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Infomation  from './components/Infomation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isExpand:false
+    }
+  }
+  onClick=()=>{
+    this.setState({
+      isExpand: !this.state.isExpand
+    })
+  }
+  render(){
+    let elementInfo = this.state.isExpand? <Infomation/> :null;
+    return (
+
+      <div>
+        <div className="divHeader">
+          <h1>Conditional Rendering</h1>
+        </div>
+        <div>
+          <button onClick={this.onClick}>
+            {this.state.isExpand?'Đóng giới thiệu':'Xem giới thiệu'}
+          </button>
+        </div>
+        {elementInfo}
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/Infomation.js
@@ -0,0 +1,14 @@
+import React , {Component} from  'react';
+
+class Infomation extends Component{
+    render(){
+        return (
+            <div>
+                <h3>Giới thiệu </h3>
+                <p>Trong React, đôi khi bạn có một sô component và tùy thuộc vào từng điều kiện ví dụ như trạng thái của state, props, ... mà bạn muốn hiển thị một hoặc một số component nào đó. Khi đó bạn có thể sử dụng Conditional rendering để render ra component mà bạn mong muốn.</p>
+            </div>
+        );
+    }
+}
+
+export default Infomation;
